6dfc4b3268b2102d3098d060a8b1905dd6384e07
Helen <hchalmers23@gmail.com>
2019-01-02 15:04:38 -0600

    on click rotate

--- Seekerz/wwwroot/js/site.js
+++ Seekerz/wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+var flipbutton = document.getElementById('flipbutton');
+
+flipbutton.addEventListener('click', function () {
+    if (!this.classList.contains('on')) {
+        this.classList.remove('off');
+        this.classList.add('on');
+    } else {
+        this.classList.remove('on');
+        this.classList.add('off');
+    }
+});
